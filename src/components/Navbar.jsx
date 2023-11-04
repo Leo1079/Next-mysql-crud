21cffd5947ccb94c283d950a0e1985440e0c60ad
Leo1079 <Leo1073@outlook.es>
2023-11-04 20:46:20 -0300

    Actualizando navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,21 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="bg-blue-500 p-4">
-      <ul className="flex space-x-4">
-        <li>
-          <Link href="/">
-            <p className="text-white">Inicio</p>
-          </Link>
-        </li>
+  <ul className="flex space-x-4 justify-between">
+    <li>
+      <Link href="/">
+        <h1 className="text-white font-bold text-xl">Market Products</h1>
+      </Link>
+    </li>
+    <li className="justify-end">
+      <Link href="/new">
+        <p className="text-white font-bold text-xl ">New</p>
+      </Link>
+    </li>
+  </ul>
+</nav>
 
-        <li>
-          <Link href="/new">
-            <p className="text-white">New</p>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+  
   );
 };
 
